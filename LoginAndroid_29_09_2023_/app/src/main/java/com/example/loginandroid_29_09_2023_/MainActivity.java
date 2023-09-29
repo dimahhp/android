7d12c06c2d35f51cc8a5058bcbda9678f8684f65
dimahhp <dima96l265@gmail.com>
2023-09-29 09:50:33 +0200

    Login_28_09

---
+++ LoginAndroid_29_09_2023_/app/src/main/java/com/example/loginandroid_29_09_2023_/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.loginandroid_29_09_2023_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
